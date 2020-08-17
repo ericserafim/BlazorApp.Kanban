125ea5a26914bf72edbf92443c6f56f00290cf56
Eric Serafim <ericserafim@gmail.com>
2020-08-17 11:13:36 -0300

    Removed template examples

--- BlazorApp.Kanban/Startup.cs
+++ BlazorApp.Kanban/Startup.cs
@@ -1,18 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using BlazorApp.Kanban.Data;
 
 namespace BlazorApp.Kanban
 {
-	public class Startup
+  public class Startup
 	{
 		public Startup(IConfiguration configuration)
 		{
@@ -26,8 +20,7 @@ namespace BlazorApp.Kanban
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddRazorPages();
-			services.AddServerSideBlazor();
-			services.AddSingleton<WeatherForecastService>();
+			services.AddServerSideBlazor();			
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
